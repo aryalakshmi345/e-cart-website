f04f7551e150631985bf5abdda8bcbe809854e97
aryalakshmi345 <lekshmiarya113@gmail.com>
2023-10-19 12:55:28 +0530

    second commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Badge, Container, Nav, Navbar } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 function Header() {
+  const wishlist = useSelector((state)=>state.wishlistReducer)
   return (
     <>
-     <Navbar expand="lg" className="bg-body-tertiary">
+     <Navbar style={{zIndex:'1'}} expand="lg" className="bg-body-tertiary position-fixed top-0 w-100 mb-5">
       <Container>
         <Navbar.Brand><Link to={'/'} style={{textDecoration:"none", color:'black'}}>E-CART</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -13,7 +15,7 @@ function Header() {
           <Nav className="ms-auto">
             <Nav.Link className='btn btn-warning border rounded'>
              <Link to={'/wishlist'} style={{textDecoration:"none", color:'black'}}><i class="me-2 fa-solid fa-heart text-danger"></i>Wishlist</Link>
-            <Badge className='ms-2' bg="dark">0</Badge>
+            <Badge className='ms-2' bg="dark">{wishlist.length}</Badge>
             </Nav.Link>
 
             <Nav.Link className='btn btn-warning border rounded ms-2'>
